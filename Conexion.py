5ff82fbf5c3290f96ffd0b9105915a27b4b4b8d0
xnz1422 <64932231+xnz1422@users.noreply.github.com>
2022-10-13 15:43:19 -0300

    Update Conexion.py
    Pruebas muchas

--- Conexion.py
+++ Conexion.py
@@ -2,3 +2,13 @@ _author_ : 'Topito'
 _email_ : 'w595440@teco.com.ar'
 USERNAME = 'w595440'
 PASSWORD = 'Ytelacreistequelaibaaponer'
+
+
+import re
+
+TEXT = 'Usuario w595440 www544440 u561107 W562211 x300092 USUARIO=x300102 USER = "topo" USR = "x300106"'
+rege = r'[\"\'\s\=][uwx][0-9]{6}'
+
+salida = re.findall(rege, TEXT, re.IGNORECASE)
+
+print(salida)
